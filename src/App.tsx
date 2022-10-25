8218685feea86cf44298967787f4cf035dc61d12
Gabriel Tadachi <gabriel.tadachi@negocieonline.com.br>
2022-10-24 21:08:41 -0300

    Aula 2.3 - Criando componente lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Botao from './components/Botao';
 import Formulario from './components/Formulario/index';
+import Lista from './components/Lista';
 
 function App() {
   return (
     <div className="App">
       <Formulario></Formulario>
+      <Lista></Lista>
     </div>
   );
 }
