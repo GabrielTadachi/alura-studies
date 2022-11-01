4a9e32f1b29f33e7d2d46512fc6967b055ea4061
Gabriel Tadachi <gabriel.tadachi@negocieonline.com.br>
2022-11-01 10:22:37 -0300

    Aula 5.2 - AdicionarTarefa

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,41 +1,56 @@
+import React from 'react';
 import Botao from '../Botao';
 import style from './Formulario.module.scss';
 
-//Class Component -> Forma Antiga!
-// class Formulario extends React.Component {
-//     render(){
-//         return(
-//             <form className={style.novaTarefa}>
-//                 <div className={style.inputContainer}>
-//                     <label htmlFor='tarefa'>Adicione um novo estudo</label>
-//                     <input type="text" name="tarefa" id="tarefa" placeholder='O que você quer estudar?' required />
-//                 </div>
-//                 <div className={style.inputContainer}>
-//                     <label htmlFor='tempo'>Tempo</label>
-//                     <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:00:00" required />
-//                 </div>
-//                 <Botao>Adicionar</Botao>
-//             </form>
-//         );
-//     }
-// }
+// Class Component -> Forma Antiga!
+class Formulario extends React.Component {
+    state = {
+        tarefa: '',
+        tempo: '00:00:00'
+    };
+    
+    adicionarTarefa(event: React.FormEvent<HTMLFormElement>){
+        event.preventDefault();
+        console.log(this.state);
+    }
 
-// export default Formulario
-
-export default function Formulario(){
-
-    return(
-        <form className={style.novaTarefa}>
-            <div className={style.inputContainer}>
-                <label htmlFor='tarefa'>Adicione um novo estudo</label>
-                <input type="text" name="tarefa" id="tarefa" placeholder='O que você quer estudar?' required />
-            </div>
-            <div className={style.inputContainer}>
-                <label htmlFor='tempo'>Tempo</label>
-                <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:00:00" required />
-            </div>
-            <Botao>Adicionar</Botao>
-        </form>
-    );
-
+    render(){
+        return(
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
+                <div className={style.inputContainer}>
+                    <label htmlFor='tarefa'>Adicione um novo estudo</label>
+                    <input type="text" value={this.state.tarefa} name="tarefa" id="tarefa" placeholder='O que você quer estudar?' required onChange={evento =>{
+                        this.setState({...this.state, tarefa: evento.target.value})
+                    }} />
+                </div>
+                <div className={style.inputContainer}>
+                    <label htmlFor='tempo'>Tempo</label>
+                    <input type="time" value={this.state.tempo} step="1" name="tempo" id="tempo" min="00:00:00" max="01:00:00" required onChange={evento =>{
+                        this.setState({...this.state, tempo: evento.target.value})
+                    }}/>
+                </div>
+                <Botao>Adicionar</Botao>
+            </form>
+        );
+    }
 }
+
+export default Formulario
+
+// export default function Formulario(){
+
+//     return(
+//         <form className={style.novaTarefa}>
+//             <div className={style.inputContainer}>
+//                 <label htmlFor='tarefa'>Adicione um novo estudo</label>
+//                 <input type="text" name="tarefa" id="tarefa" placeholder='O que você quer estudar?' required />
+//             </div>
+//             <div className={style.inputContainer}>
+//                 <label htmlFor='tempo'>Tempo</label>
+//                 <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:00:00" required />
+//             </div>
+//             <Botao>Adicionar</Botao>
+//         </form>
+//     );
+
+// }
